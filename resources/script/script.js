abef34f0ca15b43178895c8bc29968400d2b8cf9
Mxoza <Mxozai@github.com>
2021-10-09 22:47:21 +0200

    added new pages and modified style

--- resources/script/script.js
+++ resources/script/script.js
@@ -2,11 +2,25 @@ var optA;
 var optB;
 var opaq;
 var solutiontype;
-//Solution type..............................................................
+var quotekind;
+var unit_a;
+var unit_b;
+var unit_c;
+var length_a;
+var length_b;
+var length_c;
+//Solution type or Quate kind.................................................................
 optA = document.querySelector('#opt1');
 document.body.addEventListener('click', function (event) {
     if (optA.contains(event.target)) {
-        localStorage.setItem('solutiontype', 'solar');
+        if(document.location.pathname === "/solutiontype.html"){
+            localStorage.setItem('solutiontype', 'solar');
+        }
+        else if(document.location.pathname === "/quote-kind.html"){
+            localStorage.setItem('quotekind', 'broad');
+        }
+        else{
+        }
         document.getElementById("opt2").checked = false;
         if (document.getElementById('opt1').checked) {
             $("button.calc-Container").css("opacity","1.0");
@@ -19,7 +33,14 @@ document.body.addEventListener('click', function (event) {
 optB = document.querySelector('#opt2');
 document.body.addEventListener('click', function (event) {
     if (optB.contains(event.target)) {
-        localStorage.setItem('solutiontype', 'battery');
+        if(document.location.pathname === "/solutiontype.html"){
+            localStorage.setItem('solutiontype', 'battery');
+        }
+        else if(document.location.pathname === "/quote-kind.html"){
+            localStorage.setItem('quotekind', 'accurate');
+        }
+        else{
+        }
         document.getElementById("opt1").checked = false;
         if (document.getElementById('opt2').checked) {
             $("button.calc-Container").css("opacity","1.0");
@@ -28,7 +49,40 @@ document.body.addEventListener('click', function (event) {
         }
     } 
 });
-//Info.........................................................................
+unit_a = document.querySelector('.a');
+document.body.addEventListener('click', function (event) {
+    if (unit_a.contains(event.target)) {
+        document.onkeypress = function(e) {
+            checkFill();
+        };
+        document.body.onkeyup = function(e){
+            checkFill();
+        };
+    } 
+});
+unit_b = document.querySelector('.b');
+document.body.addEventListener('click', function (event) {
+    if (unit_b.contains(event.target)) {
+        document.onkeypress = function(e) {
+            checkFill();
+        };
+        document.body.onkeyup = function(e){
+            checkFill();
+        };
+    }  
+});
+unit_c = document.querySelector('.c');
+document.body.addEventListener('click', function (event) {
+    if (unit_c.contains(event.target)) {
+        document.onkeypress = function(e) {
+            checkFill();
+        };
+        document.body.onkeyup = function(e){
+            checkFill();
+        };
+    } 
+});
+//Info page..........................................................................
 
 
 var opt1 = document.querySelector('select.optA');
@@ -59,7 +113,22 @@ function checkOpt(){
          $("button.calc-Container").css("opacity","0.7");
     }
 }
-//Verify all the information required is filled.................................................
+function checkFill(){
+    length_a = document.querySelector('.a').value.length;
+    length_b = document.querySelector('.b').value.length;
+    length_c = document.querySelector('.c').value.length;
+    if(length_a > 0 && length_b > 0 && length_c > 0){
+        $("button.calc-Container").css("opacity","1.0");
+    }
+    else{
+        $("button.calc-Container").css("opacity","0.7");
+    }
+}
+//INDEX page.........................................................................
+var p = window.location.pathname;
+ if (p.length === 0 || p === "/" || p.match(/^\/?index/))
+     $("button.calc-Container").css("opacity","1.0");
+//Verify all the information required is filled..............
 function verifyAction(event){
     opaq = document.querySelector("button.calc-Container").style.opacity;
     if(opaq < 1.0){
@@ -68,7 +137,7 @@ function verifyAction(event){
     else{
         //Do nothing
     }
-    //info...........................................................
+    //info page......................................................................
     if(document.location.pathname === "/info.html"){
         solutiontype = localStorage.getItem('solutiontype');
         if(solutiontype === "solar"){
@@ -81,7 +150,7 @@ function verifyAction(event){
 
         }
     }
-    //solutiontype...........................................................
+    //solutiontype page..............................................................
     else if(document.location.pathname === "/solutiontype.html"){
         solutiontype = localStorage.getItem('solutiontype');
         if(solutiontype === "solar"){
@@ -94,16 +163,41 @@ function verifyAction(event){
 
         }
     }
-    //battery-backup...........................................................
+    //battery-backup page............................................................
     else if(document.location.pathname === "/battery-backup.html"){
         document.getElementById("info").action = "/info.html";
     }
-    //achieve...........................................................
+    //achieve page...................................................................
     else if(document.location.pathname === "/achieve.html"){
         document.getElementById("info").action = "/quote-kind.html";
     }
+    //quote page...................................................................
     else if(document.location.pathname === "/quote-kind.html"){
-        document.getElementById("info").action = "/electricity-usage.html";
+        quotekind = localStorage.getItem('quotekind');
+        if(solutiontype === "broad"){
+            document.getElementById("info").action = "/info.html";
+        }
+        else if(solutiontype === "accurate"){
+            document.getElementById("info").action = "/battery-backup.html";
+        }
+        else{
+
+        }
+    }
+    //electricity-usage page.........................................................
+    else if(document.location.pathname === "/electricity-usage.html"){
+        quotekind = localStorage.getItem('quotekind');
+        if(quotekind === "broad"){
+            document.getElementById("info").action = "/cost-confirm.html";
+        }
+        else if(quotekind === "accurate"){
+            document.getElementById("info").action = "/customise.html";
+        }
+        else{
+
+        }
+    }
+    else{
         
     }
 }
